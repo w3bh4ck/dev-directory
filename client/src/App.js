dab0ebc9dbd7fa7bc697ef8bbf23f5636638de71
w3bh4ck <amadiluckysampson@gmail.com>
2018-10-26 17:03:16 +0100

    initialized redux

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,16 @@ import Landing from './components/Landing';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 
 class App extends Component {
   render() {
     return (
+
+    <Provider store={store} >
       <Router>
         <div className="App">
           <Navbar />
@@ -22,6 +27,7 @@ class App extends Component {
           <Footer /> 
         </div>
       </Router>
+    </Provider>
     );
   }
 }
