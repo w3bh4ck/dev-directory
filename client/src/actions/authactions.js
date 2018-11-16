2148b5cb667f7d00f0b4db0536b5324eb07797da
w3bh4ck <amadiluckysampson@gmail.com>
2018-11-16 16:18:52 +0100

    started workig on the redux actions for login

--- client/src/actions/authactions.js
+++ client/src/actions/authactions.js
@@ -1,5 +1,7 @@
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 import axios from 'axios';
+import setAuthToken from '../utils/setauthtoken';
+import jwt_decode from 'jwt-decode';
 
 //register user
 export const registerUser = (userData, history) => dispatch => {
@@ -11,3 +13,32 @@ export const registerUser = (userData, history) => dispatch => {
           })
         );
 };
+
+//login - get user token
+export const loginUser = userData  => dispatch => {
+  axios.post('/api/users/login', userData)
+  .then(res => {
+    //save token to local storage
+    const { token } = res.data;
+    //set token to ls
+    localStorage.setItem('jwtToken', token)
+    //set token to the auth header
+    setAuthToken(token);
+    //decode token to get users data
+    const decoded = jwt_decode(token);
+    //set current user
+    dispatch(setCurrentUser(decoded)); 
+  }).catch(err =>dispatch({
+    type: GET_ERRORS,
+    payload: err.response.data
+  }) 
+  );
+};
+
+//set logged in user
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
